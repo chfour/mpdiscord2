eeb4bf9d242f96442eaf36fecc5b5e9a791f99bd
chfour <chfourchfour@protonmail.com>
2023-05-17 18:21:02 +0200

    updating rpc with data from mpd

--- mpdiscord.py
+++ mpdiscord.py
@@ -4,14 +4,29 @@ import json
 import asyncio
 
 
-async def update(mpd: MPDClient) -> None:
+async def update(mpd: MPDClient, rpc: AioPresence) -> None:
+    """
+        update an aiopresence with data from an mpd client
+    """
+
     status = await mpd.status()
     print(f"status: {status!r}")
     track = await mpd.currentsong()
     print(f"track: {track!r}")
 
+    if status.get("state", None) == "play":
+        await rpc.update(state=f"{track.get('artist', '?')}", details=f"{track.get('title', '?')}", large_image="unknown")
+    else:
+        await rpc.clear()
+        # rpc.close()
+        # i really wish i could close the rpc but pypresence is STUPId and wants to close the event loop
+        # also AioPresence.close() is not an async method?????
+
 
 async def main(config: dict):
+    """
+        main loop
+    """
     mpd = MPDClient()
     await mpd.connect(config["mpd"]["server"])
     print(f"connected to mpd at {config['mpd']!r}")
@@ -20,14 +35,10 @@ async def main(config: dict):
     await rpc.connect()
     print("connected to rpc")
 
-    await rpc.update(state="test", details="test", large_image="unknown")
-    # rpc.close()
-    # i really wish i could close the rpc but pypresence is STUPId and wants to close the event loop
-
-    await update(mpd)
+    await update(mpd, rpc)
     async for subsys in mpd.idle(("player",)):
         print(f"change: {subsys}")
-        await update(mpd)
+        await update(mpd, rpc)
 
 
 if __name__ == "__main__":
